b36687925c4e19863978b82c23451e57c1cb44ff
Elizabeth <mt49301600@gmail.com>
2021-10-16 17:24:18 -0400

    StoreUser

--- src/main/java/Entity/StoreUser.java
+++ src/main/java/Entity/StoreUser.java
@@ -3,12 +3,7 @@ package Entity;
 import java.util.ArrayList;
 
 public class StoreUser {
-    private static ArrayList<User> allUsers;
-
-    public StoreUser(){
-        allUsers = new ArrayList<>();
-    }
-
+    private static ArrayList<User> allUsers = new ArrayList<User>();
     public void addUser (User user){
         allUsers.add(user);
     }
