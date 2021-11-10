bf2f8da84820e7f6fb422cec0be4136942330a00
lucas-tech-1003 <688lucasl@gmail.com>
2021-11-10 00:11:35 -0500

    Added testing files for entities and usecases

---
+++ src/test/java/Entity/ParagraphPostTest.java
@@ -0,0 +1,145 @@
+package Entity;
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* ParagraphPost Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Nov 9, 2021</pre> 
+* @version 1.0 
+*/ 
+public class ParagraphPostTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: setVisibleTo(String visibleTo) 
+* 
+*/ 
+@Test
+public void testSetVisibleTo() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setLikes(int likes) 
+* 
+*/ 
+@Test
+public void testSetLikes() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setContent(String content) 
+* 
+*/ 
+@Test
+public void testSetContent() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getPOSTTIME() 
+* 
+*/ 
+@Test
+public void testGetPOSTTIME() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getVisibleTo() 
+* 
+*/ 
+@Test
+public void testGetVisibleTo() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getLOCATION() 
+* 
+*/ 
+@Test
+public void testGetLOCATION() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getLikes() 
+* 
+*/ 
+@Test
+public void testGetLikes() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getComments() 
+* 
+*/ 
+@Test
+public void testGetComments() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getContent() 
+* 
+*/ 
+@Test
+public void testGetContent() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getId() 
+* 
+*/ 
+@Test
+public void testGetId() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getPostid() 
+* 
+*/ 
+@Test
+public void testGetPostid() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getUsersWhoLiked() 
+* 
+*/ 
+@Test
+public void testGetUsersWhoLiked() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
