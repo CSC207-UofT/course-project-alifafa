04047d4cbef86aa86ea9f8cd57ab91cbfc347a36
YukiJudaiii <90449285+YukiJudaiii@users.noreply.github.com>
2021-11-10 15:13:29 -0500

    Add files via upload

---
+++ src/main/java/Entity/Group.java
@@ -0,0 +1,40 @@
+package Entity;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Group {
+    private final String GroupiD;
+    private String GroupName;
+    private ArrayList<User> members;
+    private final ArrayList<PtoGMessage> PtoGMessageHistory;
+
+    public Group(String id, String name) {
+        this.GroupiD = id;
+        this.GroupName = name;
+        this.members = new ArrayList<>();
+        this.PtoGMessageHistory = new ArrayList<>();
+    }
+
+
+    public String getGroupName() {
+        return this.GroupName;
+    }
+
+    public ArrayList<User> getMembers() {
+        return this.members;
+    }
+
+    public void addMember(User member) {
+        //Add users to the list of users in this group
+        this.members.add(member);
+    }
+
+    public void addGroupMessage(PtoGMessage message) {
+        PtoGMessageHistory.add(message);
+    }
+
+    public ArrayList<PtoGMessage> getMessage() {
+        return this.PtoGMessageHistory;
+    }
+
+}
---
+++ src/main/java/Entity/PtoGMessage.java
@@ -0,0 +1,77 @@
+package Entity;
+
+import java.time.LocalDateTime;
+
+public class PtoGMessage {
+    /**
+     * A person-to-group message.
+     */
+
+    // === Instance Variables ===
+    //The time this message created.
+    private final LocalDateTime TIME;
+    //The content of this message.
+    private final String CONTENT;
+    //The sender of this message.
+    private final User SENDER;
+    //The group of receiver of this message.
+    private final Group GROUP;
+
+
+    /**
+     * Creates a message.
+     * @param sender the sender of this message.
+     * @param content the content of this message.
+     *
+     */
+    public PtoGMessage(User  sender, Group group, String content){
+        this.SENDER = sender;
+        this.CONTENT = content;
+        this.TIME = LocalDateTime.now();
+        this.GROUP = group;
+    }
+
+
+    /**
+     * Gets the time when this message is created.
+     *
+     * @return the time when this message is created.
+     */
+    public LocalDateTime getTime(){
+        return TIME;
+    }
+
+
+    /**
+     * Gets the content of this message.
+     *
+     * @return the content of this message.
+     */
+    public String getContent(){
+        return CONTENT;
+    }
+
+
+    /**
+     * Gets the sender of this message.
+     *
+     * @return the sender of this message.
+     */
+    public User getSender(){
+        return SENDER;
+    }
+
+
+    /**
+     * Gets the receiver of this message.
+     *
+     * @return the receiver of this message.
+     */
+    public Group getGroup(){
+        return GROUP;
+    }
+
+    public String toString (){
+        return SENDER.getUserName() + ": " + CONTENT + "   (" + TIME + ")";
+    }
+}
