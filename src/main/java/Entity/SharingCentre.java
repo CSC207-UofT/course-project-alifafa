d1fe90a5352df29f4f9a3d48489f18b6d0be47bb
lucas-tech-1003 <688lucasl@gmail.com>
2021-11-11 18:44:31 -0500

    Added doctrings to Notifications, PostsManager, and SharingCentre.

--- src/main/java/Entity/SharingCentre.java
+++ src/main/java/Entity/SharingCentre.java
@@ -4,10 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SharingCentre {
+    /**
+     * A place that contains the posts posted by the user and user's friends,
+     * and all notifications the owner of the sharing centre received.
+     */
+
     private final List<ParagraphPost> allPosts;
     private final List<Notifications> notificationList;
     private boolean newPostNotification;
 
+    /**
+     * A constructor of sharingCentre.
+     */
     public SharingCentre() {
         this.allPosts = new ArrayList<ParagraphPost>();
         this.notificationList = new ArrayList<Notifications>();
--- src/main/java/UseCase/PostsManager.java
+++ src/main/java/UseCase/PostsManager.java
@@ -14,6 +14,14 @@ public class PostsManager {
      * post.
      */
 
+    /**
+     * Creates a post.
+     * @param content Content of the post (could be none only if pictures is non-empty)
+     * @param location Location
+     * @param pictures List of pictures that the user wants to add
+     *                (could be none)
+     * @return Either a PicturePost or a ParagraphPost
+     */
     private ParagraphPost createPost(String content, String location, List<File> pictures){
         if (!pictures.isEmpty()) {
             return new PicturePost(LocalDateTime.now(), location, content, pictures);
@@ -21,6 +29,16 @@ public class PostsManager {
         return new ParagraphPost(LocalDateTime.now(), location, content);
     }
 
+    /**
+     * Posting a post. (But cannot be meaningless where it has no content and
+     *                  no pictures)
+     * Also notify all user's friends.
+     * @param user The user who wants to post
+     * @param content Content of the post (could be none only if pictures is non-empty)
+     * @param location Location
+     * @param pictures List of pictures that the user wants to add
+     *                 (could be none)
+     */
     public void postAPost(User user, String content, String location, List<File> pictures){
         ParagraphPost post = createPost(content, location, pictures);
         user.getSharingCentre().getAllPosts().add(post);
@@ -32,6 +50,12 @@ public class PostsManager {
         }
     }
 
+    /**
+     * Deleting a specific post from user's collection of posts and removes
+     * the post from the sharing centre of the user and the user's friends.
+     * @param user The user who wants to delete a post his/her posts.
+     * @param post The specific post the user wants to delete.
+     */
     public void deletePost(User user, ParagraphPost post){
         user.getSharingCentre().getAllPosts().remove(post);
         user.getMyPosts().remove(post);
@@ -40,6 +64,12 @@ public class PostsManager {
         }
     }
 
+    /**
+     * Like a post and increase total likes by 1. Add notification to the post
+     * creator.
+     * @param user The user who liked the post
+     * @param post The post liked
+     */
     public void likePost(User user, ParagraphPost post){
         post.setLikes(post.getLikes() + 1);
         post.getUsersWhoLiked().add(user);
@@ -47,6 +77,13 @@ public class PostsManager {
         addNotification(user, post, notification);
     }
 
+    /**
+     * Adds notification to the post creator, which is a user.
+     * @param user Add notification to either this user or a user's friend
+     * @param post If the user's all posts contain this post, then we add the
+     *             notification to the post creator
+     * @param notification A notification to be added
+     */
     private void addNotification(User user, ParagraphPost post, Notifications notification) {
         if (user.getMyPosts().contains(post)){
             user.getSharingCentre().getNotificationList().add(notification);
@@ -71,20 +108,42 @@ public class PostsManager {
 
     }
 
+    /**
+     * Add a comment from the user who commented to the post and add
+     * notification to the post creator.
+     * @param user Who commented
+     * @param post The post that's commented on
+     * @param comment Comment the commenter wrote to the post
+     */
     public void commentPost(User user, ParagraphPost post, String comment){
         addElementToHashMap(user, post.getComments(), comment);
         Notifications notification = new Notifications(user, LocalDateTime.now(), post, false);
         addNotification(user, post, notification);
     }
 
+    /**
+     * Returns all user's posts that he/she currently possess.
+     * @param user The user who to retrieve posts from
+     * @return All the post user has created (excluding deleted posts)
+     */
     public List<ParagraphPost> retrieveUsersAllPosts(User user) {
         return user.getMyPosts();
     }
 
+    /**
+     * Returns all user's posts inside his/her sharing centre.
+     * @param user The user who to retrieve posts from
+     * @return All the post from a particular user's sharing centre
+     */
     public List<ParagraphPost> retrieveSharingCentre(User user) {
         return user.getSharingCentre().getAllPosts();
     }
 
+    /**
+     * Returns all user's notifications in his/her sharing centre.
+     * @param user The user who to retrieve notifications from
+     * @return All notifications from this user's sharing centre
+     */
     public List<Notifications> retrieveNotifications(User user) {
         return user.getSharingCentre().getNotificationList();
     }
