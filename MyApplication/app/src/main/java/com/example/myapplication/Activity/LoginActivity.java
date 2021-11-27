a62a8d282c4574908d233fb8a82c0126a8d13fc6
Stephc0211 <Stephc0211@gmail.com>
2021-11-27 17:24:18 -0500

    Copied code from original project.

--- MyApplication/app/src/main/java/com/example/myapplication/Activity/LoginActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Activity/LoginActivity.java
@@ -1,15 +1,58 @@
 package com.example.myapplication.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.myapplication.Controllers.UserControllers.LogInController;
+import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText mEtUsername;
+    private EditText mEtPassword;
+    private Button mBtnLogin;
+    private TextView mTvRegistration;
+
+    private boolean loggedIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        LogInController logInController = new LogInController();
+
+        mEtPassword = findViewById(R.id.et_password);
+        mEtUsername = findViewById(R.id.et_username);
+        mBtnLogin = findViewById(R.id.btn_login);
+
+        mBtnLogin.setOnClickListener(view -> {
+            mEtUsername.getText();
+            mEtPassword.getText();
+
+            loggedIn = logInController.runLogIn(mEtUsername, mEtPassword);
+            if (loggedIn){
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }else{
+                Toast.makeText(LoginActivity.this, "Please type the correct username or password!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        mTvRegistration = findViewById(R.id.tv_register);
+        mTvRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,102 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.example.myapplication.Fragment.AliChatFragment;
+import com.example.myapplication.Fragment.ContactFragment;
+import com.example.myapplication.Fragment.ProfileFragment;
+import com.example.myapplication.Fragment.SharingCentreFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
+
+    private FrameLayout mFlMainContainer;
+    private AliChatFragment mAliChatFragment;
+    private ContactFragment mContactFragment;
+    private ProfileFragment mProfileFragment;
+    private SharingCentreFragment mSharingCentreFragment;
+
+    private RadioButton mRbAliChat;
+    private RadioButton mRbContact;
+    private RadioButton mRbSharingCentre;
+    private RadioButton mRbProfile;
+    private RadioGroup mRgMain;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFlMainContainer = findViewById(R.id.fl_main_container);
+        mRbAliChat = findViewById(R.id.rb_ali_chat);
+        mRbContact = findViewById(R.id.rb_contact);
+        mRbSharingCentre = findViewById(R.id.rb_sharing_centre);
+        mRbProfile = findViewById(R.id.rb_profile);
+        mRgMain = findViewById(R.id.rg_main);
+
+        mAliChatFragment = new AliChatFragment();
+        mContactFragment = new ContactFragment();
+        mSharingCentreFragment = new SharingCentreFragment();
+        mProfileFragment = new ProfileFragment();
+
+        mRgMain.setOnCheckedChangeListener(this);
+
+        mRbAliChat.setChecked(true);
+        replaceFragment(mAliChatFragment);
+
+
+    }
+
+    public void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fl_main_container, fragment);
+        fragmentTransaction.commit();
+    }
+
+    public void onCheckedChanged(RadioGroup radioGroup, int i){
+        switch (i) {
+            case R.id.rb_ali_chat:
+                if (null != mAliChatFragment){
+                    mAliChatFragment = new AliChatFragment();
+                }
+                replaceFragment(mAliChatFragment);
+                break;
+
+            case R.id.rb_contact:
+                if (null != mContactFragment){
+                    mContactFragment = new ContactFragment();
+                }
+                replaceFragment(mContactFragment);
+                break;
+
+            case R.id.rb_sharing_centre:
+                if (null != mSharingCentreFragment){
+                    mSharingCentreFragment = new SharingCentreFragment();
+                }
+                replaceFragment(mSharingCentreFragment);
+                break;
+
+            case R.id.rb_profile:
+                if (null != mProfileFragment){
+                    mProfileFragment = new ProfileFragment();
+                }
+                replaceFragment(mProfileFragment);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
