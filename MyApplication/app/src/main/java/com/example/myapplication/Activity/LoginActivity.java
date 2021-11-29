088580893f96957062149e7b13afc2d441410e5b
Stephc0211 <Stephc0211@gmail.com>
2021-11-28 22:56:58 -0500

    Copied code from original project.

--- MyApplication/app/src/main/java/com/example/myapplication/Activity/LoginActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Activity/LoginActivity.java
@@ -2,7 +2,6 @@ package com.example.myapplication.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -46,12 +45,9 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
         mTvRegistration = findViewById(R.id.tv_register);
-        mTvRegistration.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
-            }
+        mTvRegistration.setOnClickListener(view -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
         });
 
     }
