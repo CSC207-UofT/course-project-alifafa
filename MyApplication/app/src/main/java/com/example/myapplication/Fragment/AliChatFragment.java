96e36e77a9377f3902d1dd406af3e9e9069e5c67
grace-fengqing-yu <fqgrace.hi@gmail.com>
2021-12-01 20:34:10 -0500

    random commit (delete this)

--- MyApplication/app/src/main/java/com/example/myapplication/Fragment/AliChatFragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Fragment/AliChatFragment.java
@@ -14,8 +14,7 @@ public class AliChatFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_ali_chat, container, false);
-        return view;
+        return inflater.inflate(R.layout.fragment_ali_chat, container, false);
     }
 
 }
