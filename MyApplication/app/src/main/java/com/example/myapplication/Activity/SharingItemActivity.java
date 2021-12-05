1ba5aa11a256a0a489e98b444e2f74bc9b6632d0
Lucas Lei <huipeng.lei@mail.utoronto.ca>
2021-12-05 04:59:19 -0500

    Added xml files for sharingcentre: include_item_comment.xml, include_item_gridview.xml, fragment_sharing_centre.xml, activity_sharing_item.xml.

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Activity/SharingItemActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.Activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.myapplication.R;
+
+public class SharingItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sharing_item);
+    }
+}
