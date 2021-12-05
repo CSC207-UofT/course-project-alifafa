f47d61cda7a85e8b6567350c6e63c58d7278128c
Lucas Lei <huipeng.lei@mail.utoronto.ca>
2021-12-05 17:38:04 -0500

    Modified SharingCentreFragment.java. Added new recyclerview adapter for SharingCentreFragment

--- MyApplication/app/src/main/java/com/example/myapplication/Fragment/SharingCentreFragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Fragment/SharingCentreFragment.java
@@ -8,13 +8,36 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.myapplication.Adapter.SharingCentreFragmentAdapter;
+import com.example.myapplication.Entity.ParagraphPost;
 import com.example.myapplication.R;
 
+import java.util.List;
+
 public class SharingCentreFragment extends Fragment {
+
+    private RecyclerView mRVPosts;
+    public static List<String[]> postInfo;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sharing_centre, container, false);
+
+        // Try out
+        for (int i = 0; i < 10; i++) {
+            String[] temp = {"user"+i, "content"+i, "location", "1"};
+            postInfo.add(temp);
+        }
+
+        mRVPosts = view.findViewById(R.id.rv_sharing_centre);
+        // Use an adapter to display item
+        mRVPosts.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        mRVPosts.setAdapter(new SharingCentreFragmentAdapter(view.getContext()));
+
         return view;
     }
+
 }
