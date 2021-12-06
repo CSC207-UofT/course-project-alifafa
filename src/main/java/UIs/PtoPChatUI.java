c991f49b1c52c040942ef92c2dc5dcc13b9f0de7
grace-fengqing-yu <fqgrace.hi@gmail.com>
2021-12-06 00:49:52 -0500

    The program now check if the user is logged in before any further execution.

--- src/main/java/UIs/PtoPChatUI.java
+++ src/main/java/UIs/PtoPChatUI.java
@@ -1,5 +1,6 @@
 package UIs;
 
+import CommandControl.Constants;
 import Controllers.ChatControllers.PtoPMessageControllerFacade;
 import Controllers.UserControllers.CheckFriendController;
 import Controllers.UserControllers.FindLoggedInUserController;
@@ -18,45 +19,54 @@ public class PtoPChatUI extends ParentUI{
 
     // Use this method to realize person to person chat function.
     public void run () throws IOException {
+        Constants constants = new Constants();
 
-        PtoPMessageControllerFacade controller = new PtoPMessageControllerFacade();
-        PtoPMessageHistoryPresenter presenter = new PtoPMessageHistoryPresenter();
+        String currentUser = constants.getCurrentUser();
 
-        CheckFriendController checkFriendController = new CheckFriendController();
-        CheckFriendPresenter checkFriendPresenter = new CheckFriendPresenter();
+        if (currentUser==null){
+            System.out.println("You need to log in first!");
+        } else {
 
-        FindLoggedInUserPresenter findLoggedInUserPresenter = new FindLoggedInUserPresenter();
-        FindLoggedInUserController findLoggedInUserController = new FindLoggedInUserController();
+            PtoPMessageControllerFacade controller = new PtoPMessageControllerFacade();
+            PtoPMessageHistoryPresenter presenter = new PtoPMessageHistoryPresenter();
 
-        findLoggedInUserController.findLoggedInUser(findLoggedInUserPresenter);
-        String userName = findLoggedInUserPresenter.presentOutput();
+            CheckFriendController checkFriendController = new CheckFriendController();
+            CheckFriendPresenter checkFriendPresenter = new CheckFriendPresenter();
+
+            FindLoggedInUserPresenter findLoggedInUserPresenter = new FindLoggedInUserPresenter();
+            FindLoggedInUserController findLoggedInUserController = new FindLoggedInUserController();
+
+            findLoggedInUserController.findLoggedInUser(findLoggedInUserPresenter);
+            String userName = findLoggedInUserPresenter.presentOutput();
 
 
-        String[] parameters = new String[2];
+            String[] parameters = new String[2];
 
-        System.out.println("Friend's UserName: ");
-        Scanner scanner = new Scanner(System.in);
-        parameters[0] = scanner.nextLine();
+            System.out.println("Friend's UserName: ");
+            Scanner scanner = new Scanner(System.in);
+            parameters[0] = scanner.nextLine();
 
-        checkFriendController.checkFriend(userName, parameters[0], checkFriendPresenter);
-        boolean isFriend = checkFriendPresenter.isFriend();
+            checkFriendController.checkFriend(userName, parameters[0], checkFriendPresenter);
+            boolean isFriend = checkFriendPresenter.isFriend();
 
-        if (isFriend){
-        //print previous chat history between my friend and me.
-        System.out.println("Previous chat history: ");
-        controller.receiveMessageHistory(userName, parameters[0], presenter);
-        presenter.present();
+            if (isFriend) {
+                //print previous chat history between my friend and me.
+                System.out.println("Previous chat history: ");
+                controller.receiveMessageHistory(userName, parameters[0], presenter);
+                presenter.present();
 
-        //send message to the friend
-        System.out.println("Please type your message.");
-        parameters[1] = scanner.nextLine();
-        controller.sendMessage(userName, parameters[0], parameters[1]);
+                //send message to the friend
+                System.out.println("Please type your message.");
+                parameters[1] = scanner.nextLine();
+                controller.sendMessage(userName, parameters[0], parameters[1]);
 
-        //print latest chat history between my friend and me.
-        System.out.println("Current chat history: ");
-        controller.receiveMessageHistory(userName, parameters[0], presenter);
-        presenter.present();}else{
-            System.out.println("Not valid friend");
+                //print latest chat history between my friend and me.
+                System.out.println("Current chat history: ");
+                controller.receiveMessageHistory(userName, parameters[0], presenter);
+                presenter.present();
+            } else {
+                System.out.println("Not valid friend");
+            }
         }
 
     }
