a8747eff4fd69d237b5d3c91241bb0e717bc1e9c
zzqjames <zhiqi.zhao@mail.utoronto.ca>
2021-12-08 02:56:19 -0500

    added comments for all input boundaries

--- src/main/java/InputBoundary/GroupInputBoundary.java
+++ src/main/java/InputBoundary/GroupInputBoundary.java
@@ -6,19 +6,43 @@ import OutputBoundary.*;
 import java.io.IOException;
 
 public interface GroupInputBoundary {
-    /**
-     * This InputBoundary interface must be implemented by the GroupManager.
-     * Controller knows nothing about the GroupManager but it can use InputBoundary instead.
-     */
 
+     // This InputBoundary interface must be implemented by the GroupManager.
+     // Controller knows nothing about the GroupManager but it can use InputBoundary instead.
+
+
+
+    /**
+     * @param parameters The inputs for runCreateGroup
+     * @param outputBoundary the outputBoundary for GroupInputBoundary
+     * @throws IOException exception
+     */
     void runCreateGroup(String[] parameters, CreateGroupOutputBoundary outputBoundary) throws IOException;
 
+    /**
+     * @param parameters The inputs for runJoinGroup
+     * @param outputBoundary the outputBoundary for GroupInputBoundary
+     * @throws IOException exception
+     */
     void runJoinGroup(String[] parameters, JoinGroupOutputBoundary outputBoundary) throws IOException;
 
+    /**
+     * @param user The user of runCheckGroup
+     * @param group The group of runCheckGroup
+     * @param outputBoundary the outputBoundary for GroupInputBoundary
+     */
     void runCheckGroup(String user, String group, CheckGroupOutputBoundary outputBoundary);
 
+    /**
+     * @param id the group id
+     * @return return id
+     */
     Group getGroup(String id);
 
+    /**
+     * @param parameters The inputs for runKickGroupMember
+     * @param outputBoundary the outputBoundary for GroupInputBoundary
+     */
     void runKickGroupMember(String[] parameters, KickGroupMemberOutputBoundary outputBoundary);
 
 }
--- src/main/java/InputBoundary/UserInputBoundary.java
+++ src/main/java/InputBoundary/UserInputBoundary.java
@@ -7,30 +7,72 @@ import java.io.IOException;
 
 
 public interface UserInputBoundary {
-    /**
-     * This InputBoundary interface must be implemented by the UserManager.
-     * Controller knows nothing about the UserManager, but it can use InputBoundary instead.
-     */
+     // This InputBoundary interface must be implemented by the UserManager.
+     // Controller knows nothing about the UserManager, but it can use InputBoundary instead.
 
+
+    /**
+     * @param parameters the inputs of runLogIn
+     * @param outputBoundary the outputBoundary of UserManager
+     */
     void runLogIn (String[] parameters, LogInOutputBoundary outputBoundary);
 
+    /**
+     * @param parameters the inputs of runAccountRegistration
+     * @param outputBoundary the outputBoundary of UserManager
+     * @throws IOException exception
+     */
     void runAccountRegistration (String[] parameters, AccountRegistrationOutputBoundary outputBoundary) throws IOException;
 
+    /**
+     * @param parameters the inputs of runAddFriend
+     * @param outputBoundary the outputBoundary of UserManager
+     * @throws IOException exception
+     */
     void runAddFriend(String[] parameters, AddFriendOutputBoundary outputBoundary) throws IOException;
 
+    /**
+     * @param id user ID
+     * @return return a user
+     */
     User getUser (String id);
 
+    /**
+     * @param me the user
+     * @param friend user's friend
+     * @param outputBoundary the outputBoundary of UserManager
+     */
     void runCheckFriend(String me, String friend, CheckFriendOutputBoundary outputBoundary);
 
+    /**
+     * @param username  the user's UserName
+     */
     void runLogOut(String username);
 
     void findLoggedInUser(LogOutOutputBoundary logOutOutputBoundary);
 
+    /**
+     * @param parameters the inputs of runRemoveFriend
+     * @param outputBoundary the outputBoundary of UserManager
+     */
     void runRemoveFriend(String[] parameters, RemoveFriendOutputBoundary outputBoundary);
 
+    /**
+     * @param parameters the inputs of runAddBlocked
+     * @param outputBoundary the outputBoundary of UserManager
+     * @throws IOException exception
+     */
     void runAddBlocked(String[] parameters, BlockedListOutputBoundary outputBoundary) throws IOException;
 
+    /**
+     * @param parameters the inputs of runCheckBlocked
+     * @return if is blocked
+     */
     boolean runCheckBlocked(String[] parameters);
 
+    /**
+     * @param parameters the inputs of runEditPassword
+     * @param outputBoundary the outputBoundary of UserManager
+     */
     void runEditPassword(String[] parameters, EditPasswordOutputBoundary outputBoundary);
 }
