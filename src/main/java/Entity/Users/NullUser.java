2a9c29dcff8d6bf8c00f3f982d8111f7bfd7b09e
Stephc0211 <Stephc0211@gmail.com>
2021-12-08 20:03:26 -0500

    Added null pattern

---
+++ src/main/java/Entity/Users/NullUser.java
@@ -0,0 +1,13 @@
+package Entity.Users;
+
+import Entity.Post.ParagraphPost;
+import Entity.Post.SharingCentre;
+
+import java.util.ArrayList;
+
+public class NullUser extends User {
+
+    public NullUser() {
+        super();
+    }
+}
