ad867fe32cc0332e516f3d8f1e78d829562796e7
Lucas Lei <huipeng.lei@mail.utoronto.ca>
2021-12-08 22:02:19 -0500

    Added tests for controllers of post

---
+++ src/test/java/Controllers/PostsSharingController/PostCommentAndLike/DeletePostControllerTest.java
@@ -0,0 +1,12 @@
+package Controllers.PostsSharingController.PostCommentAndLike;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeletePostControllerTest {
+
+    @Test
+    void runDeletePost() {
+    }
+}
